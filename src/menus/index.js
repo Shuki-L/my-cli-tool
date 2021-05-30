89031260905a2ee99e7da5e9c83b043d9569fdbc
Shuki Levy <levy.shuki@gmail.com>
2021-05-30 11:24:22 +0300

    handle go back for dynamic

--- src/menus/index.js
+++ src/menus/index.js
@@ -2,38 +2,44 @@ const inquirer = require("inquirer");
 const { getCommonMenu } = require("./common");
 const main = require("./main");
 const localstack = require("./localstack");
+const { MENUS } = require("../constants");
 
 const getMenu = (menuName, ctx, menus = null) => {
-    //todo inject custom menu
-    menus = menus || { ...main, ...localstack };
+    try {
+        //todo inject custom menu
+        menus = menus || { ...main, ...localstack };
 
-    const { items, meta = {} } = menus[menuName];
+        const { items, meta = {} } = menus[menuName];
 
-    ctx.choises = items;
-    ctx.isMainMenu = meta.isMain || false;
+        ctx.choises = items;
+        ctx.isMainMenu = meta.isMain || false;
 
-    ctx.previousMenu = meta.isMain ? null : ctx.currentMenu;
-    ctx.currentMenu = menuName;
+        ctx.previousMenu = meta.isMain ? null : ctx.currentMenu;
+        ctx.currentMenu = menuName;
 
-    choices = [
-        ...(ctx.choises ? ctx.choises : []),
-        ...[new inquirer.Separator()],
-        ...getCommonMenu(ctx),
-    ];
+        choices = [
+            ...(ctx.choises ? ctx.choises : []),
+            ...[new inquirer.Separator()],
+            ...getCommonMenu(ctx),
+        ];
 
-    return inquirer
-        .prompt([
-            {
-                type: "rawlist",
-                name: "choice",
-                message: ctx.menuMessage || "Select option",
-                loop: false,
-                choices: choices,
-            },
-        ])
-        .then((answers) => {
-            ctx.performAction(answers, ctx);
-        });
+        return inquirer
+            .prompt([
+                {
+                    type: "rawlist",
+                    name: "choice",
+                    message: ctx.menuMessage || "Select option",
+                    loop: false,
+                    choices: choices,
+                },
+            ])
+            .then((answers) => {
+                ctx.performAction(answers, ctx);
+            });
+    } catch (error) {
+        // todo - handle dynamic menu items
+        getMenu(MENUS.MAIN, ctx);
+    }
 };
 
 module.exports = getMenu;
