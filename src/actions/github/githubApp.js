80bd52cf537fcfe7d3a67aefd9d59b017b5464e8
Shuki Levy <shuki@jit.io>
2022-04-25 00:08:45 +0300

    add table

--- src/actions/github/githubApp.js
+++ src/actions/github/githubApp.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 const chalk = require("chalk");
+const { Table } = require("console-table-printer");
+
 const { createAppAuth } = require("@octokit/auth-app");
 const { request } = require("@octokit/request");
 const input = require("@inquirer/input");
@@ -40,11 +42,76 @@ const getAppInstallations = async (ctx) => {
     });
     if (result.status === 200) {
       const installations = result.data;
-      console.log(chalk.yellow.bold("App Installations:"));
-      console.info(installations);
-      console.log(
-        chalk.green.bold(`Total installations: ${installations.length}`)
-      );
+      if (installations.length == 0) {
+        console.log(chalk.yellow.bold("No installations found"));
+      } else {
+        const appName = `${installations[0].app_slug}(${installations[0].app_id})`;
+        console.log(chalk.yellow.bold(`${appName} Installations:`));
+
+        const orgName = installations[0].account.login;
+        const t = new Table({
+          columns: [
+            {
+              name: "id",
+              title: "Installation ID",
+              alignment: "left",
+              color: "green",
+            },
+            {
+              name: "orgName",
+              title: "Org Name",
+              alignment: "left",
+              color: "yellow",
+            },
+            {
+              name: "orgType",
+              title: "Org Type",
+              alignment: "left",
+              color: "yellow",
+            },
+            {
+              name: "createdAt",
+              title: "Created At",
+              alignment: "left",
+              color: "green",
+            },
+            {
+              name: "updatedAt",
+              title: "Updated At",
+              alignment: "left",
+              color: "yellow",
+            },
+          ],
+        });
+
+        for (const installation of installations) {
+          t.addRow({
+            id: installation.id,
+            orgName: installation.account.login,
+            orgType: installation.account.type,
+            createdAt: installation.created_at,
+            updatedAt: installation.updated_at,
+          });
+        }
+
+        t.printTable();
+        console.log(
+          chalk.green.bold(`Total installations: ${installations.length}`)
+        );
+        const showFull = await input({
+          message: "Do you want to see full installations details (y/n)? ",
+        });
+
+        if (
+          showFull.toLowerCase() === "yes" ||
+          showFull.toLowerCase() === "y"
+        ) {
+          console.info(installations);
+          console.log(
+            chalk.green.bold(`Total installations: ${installations.length}`)
+          );
+        }
+      }
     } else {
       console.log(chalk.red.bold("Error: " + result.status));
       console.log(chalk.red.bold(result.data));
